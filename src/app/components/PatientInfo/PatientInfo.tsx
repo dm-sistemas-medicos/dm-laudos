b0e03525a26db4dd7a9171687676e86b6c5ebab3
Daniel Xavier <dxdanielxavier@gmail.com>
2024-04-19 17:40:53 -0300

    refactor: use InfoInput component in PatientInfo

--- src/app/components/PatientInfo/PatientInfo.tsx
+++ src/app/components/PatientInfo/PatientInfo.tsx
@@ -2,26 +2,20 @@
 
 import { useEffect, useState } from "react";
 
+import InfoInput from "./components/InfoInput/InfoInput";
+
 import styles from "./PatientInfo.module.scss";
 
+const today = new Date();
+
 const PatientInfo = () => {
-  const today = new Date();
-
-  const [name, setName] = useState("");
-
-  const [medicalRecordNumber, setMedicalRecordNumber] = useState("");
-
   const [medicalReportDate, setMedicalReportDate] = useState(
     today.toISOString().substring(0, 10)
   );
 
-  const [requesterName, setRequesterName] = useState("DRA MARIANA MACHOSKI");
-
   const [birthdate, setBirthdate] = useState("");
 
   const [lifetime, setLifetime] = useState("");
-
-  const [operatorName, setOperatorName] = useState("DRA MARIANA MACHOSKI");
 
   const [medicalInsuranceName, setMedicalInsuranceName] = useState("");
 
@@ -46,26 +40,12 @@ const PatientInfo = () => {
 
   return (
     <form className={styles["patient-info"]}>
-      <div>
-        <label htmlFor="name">Nome:</label>
-        <input
-          id="name"
-          type="text"
-          value={name}
-          onChange={({ target: { value } }) => setName(value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="medicalRecordNumber">Prontuário:</label>
-        <input
-          className={styles["small-input"]}
-          id="medicalRecordNumber"
-          type="text"
-          width={48}
-          value={medicalRecordNumber}
-          onChange={({ target: { value } }) => setMedicalRecordNumber(value)}
-        />
-      </div>
+      <InfoInput id="name" label="Nome" />
+      <InfoInput
+        id="medicalRecordNumber"
+        label="Prontuário"
+        inputSize="small"
+      />
       <div>
         <label htmlFor="medicalReportDate">Data:</label>
         <input
@@ -76,15 +56,11 @@ const PatientInfo = () => {
           onChange={({ target: { value } }) => setMedicalReportDate(value)}
         />
       </div>
-      <div>
-        <label htmlFor="requesterName">Solicitante:</label>
-        <input
-          id="requesterName"
-          type="text"
-          value={requesterName}
-          onChange={({ target: { value } }) => setRequesterName(value)}
-        />
-      </div>
+      <InfoInput
+        id="requesterName"
+        label="Solicitante"
+        initialInfo="DRA MARIANA MACHOSKI"
+      />
       <div className={styles["extra-info"]}>
         <label htmlFor="birthdate">Nascimento:</label>
         <input
@@ -96,15 +72,11 @@ const PatientInfo = () => {
         />
         <span>{lifetime}</span>
       </div>
-      <div>
-        <label htmlFor="operatorName">Operador:</label>
-        <input
-          id="operatorName"
-          type="text"
-          value={operatorName}
-          onChange={({ target: { value } }) => setOperatorName(value)}
-        />
-      </div>
+      <InfoInput
+        id="operatorName"
+        label="Operador"
+        initialInfo="DRA MARIANA MACHOSKI"
+      />
       <div className={styles["extra-info"]}>
         <label htmlFor="medicalInsuranceName">Convênio:</label>
         <input
