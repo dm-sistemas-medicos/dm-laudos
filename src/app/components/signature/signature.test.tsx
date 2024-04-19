60ba57f1c2e2d637a9a68217761d87e0505a2212
Daniel Xavier <dxdanielxavier@gmail.com>
2024-04-19 14:50:15 -0300

    test: add Signature component tests

---
+++ src/app/components/signature/signature.test.tsx
@@ -0,0 +1,37 @@
+import "@testing-library/jest-dom";
+
+import { render, screen } from "@testing-library/react";
+
+import Signature from "./signature";
+
+describe("Signature component", () => {
+  beforeEach(() => {
+    render(<Signature />);
+  });
+
+  it("renders the image", () => {
+    const image = screen.getByAltText("Assinatura");
+
+    expect(image).toBeInTheDocument();
+  });
+
+  it("renders the doctor name", () => {
+    const doctorName = screen.getByText("Dra. Mariana Carolina C. Machoski");
+
+    expect(doctorName).toBeInTheDocument();
+  });
+
+  it("renders the doctor speciality", () => {
+    const doctorSpeciality = screen.getByText("Médica Otorrinolaringologista");
+
+    expect(doctorSpeciality).toBeInTheDocument();
+  });
+
+  it("renders the doctor registration numbers", () => {
+    const doctorRegistrationNumbers = screen.getByText(
+      "CRM-SC 35.098 | RQE 23.991"
+    );
+
+    expect(doctorRegistrationNumbers).toBeInTheDocument();
+  });
+});
