e79f127025464f9bebf669d07423e31c3254d687
Daniel Xavier <dxdanielxavier@gmail.com>
2024-04-23 15:24:01 -0300

    tests: add coverageThreshold baseline

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,14 @@ const config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/app/layout.tsx"],
+  coverageThreshold: {
+    global: {
+      statements: 85.71,
+      branches: 97.91,
+      lines: 85.71,
+      functions: 71.42,
+    },
+  },
 };
 
 module.exports = createJestConfig(config);
