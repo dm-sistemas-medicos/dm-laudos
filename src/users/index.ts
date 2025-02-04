ccd6fb82f5098b3decd5fc917372309729297a6c
Daniel Xavier <dxdanielxavier@gmail.com>
2025-02-03 21:48:54 -0300

    feat: added leticia-leahy user

--- src/users/index.ts
+++ src/users/index.ts
@@ -1,7 +1,9 @@
 import MarianaMachoski from "./mariana-machoski/user-info.json";
+import LeticiaLeahy from "./leticia-leahy/user-info.json";
 
 const users = {
   "mariana-machoski": MarianaMachoski,
+  "leticia-leahy": LeticiaLeahy,
 };
 
 export default users;
